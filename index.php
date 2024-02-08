11fa8d8dddddc60c8e4fc5ef3266ffb9fa8f15b2
DomenicoScala <dome.scala12@gmail.com>
2024-02-08 16:13:36 +0100

    MILESTONE 1 COMPLETA

---
+++ index.php
@@ -0,0 +1,102 @@
+<?php     
+    $password= '';
+    if(isset($_GET['length'])){
+        $passLength = intval($_GET['length']);
+
+        if($passLength >= 3 && $passLength <= 18){
+            $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!$%&()=?_-[]';
+            $min= 0;
+            $max= strlen($characters) - 1;
+
+
+            
+            for($i = 0; $i < $passLength; $i++){
+                $randomCharacter = $characters[mt_rand($min,$max)];
+
+                $password .= $randomCharacter;
+            };         
+        };
+    }
+?>
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Password Generator</title>
+
+        <!-- BOOTSTRAP -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="./css/style.css">
+
+
+    </head>
+    <body>
+        
+        <header>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h1 class="text-center my-5">
+                            PASSWORD GENERATOR
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <main>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <form class="row" action="" method="GET">
+                            <div class="mb-3">
+                                <label for="inputPassword2" class="visually-hidden">
+                                    Lunghezza Password
+                                </label>
+
+                                <input type="number" 
+                                    name="length" 
+                                    class="form-control" 
+                                    id="inputPassword2" 
+                                    placeholder="Inserisci la lunghezza della password"
+                                    required
+                                    min="3">
+                            </div>
+                            <div class="">
+                                <button type="submit" class="btn btn-primary mb-3">INVIA</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <h2>
+                            LA PASSWORD GENERATA E':
+                            <strong>
+                                <?php 
+                                    echo $password
+                                ?>
+                            </strong>
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>        
+    </body>
+</html>
